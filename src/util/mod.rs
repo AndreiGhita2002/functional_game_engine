a8527aa4b5bcdea78308e6b89abc4066a7e67533
andrei <andrei.ghita2002@gmail.com>
2024-06-16 00:38:32 +0100

    created the Byteable trait

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,8 +1,45 @@
+use std::intrinsics::transmute;
+use std::ptr;
+use bytemuck::from_bytes;
+use mem_macros::size_of;
+
 pub mod arena;
 pub mod res;
-mod anything;
+pub mod mutators;
 
 pub enum Either<T1, T2> {
     This(T1),
     That(T2),
+}
+
+pub trait Byteable {
+    fn from_bytes(bytes: &[u8]) -> Self;
+
+    fn from_vec_bytes(vec: Vec<u8>) -> Self;
+
+    fn into_bytes(self) -> Vec<u8>;
+}
+
+impl<T> Byteable for T {
+    fn from_bytes(bytes: &[u8]) -> Self {
+        assert_eq!(bytes.len(), size_of!(T));
+        let p: *const u8 = bytes.as_ptr();
+        unsafe {
+            ptr::read(transmute::<*const u8, *const T>(p))
+        }
+    }
+
+    #[inline]
+    fn from_vec_bytes(vec: Vec<u8>) -> Self {
+        from_bytes(vec.as_slice())
+    }
+
+    fn into_bytes(self) -> Vec<u8> {
+        let p: *const T = &self;
+        let slice = unsafe {
+            let t_slice = std::slice::from_raw_parts(p, size_of!(T));
+            transmute::<&[T], &[u8]>(t_slice)
+        };
+        Vec::from(slice)
+    }
 }
